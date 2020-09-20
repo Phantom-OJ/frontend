458d9051fc7eee4b5bd013011d832310ac804b48
Star-Sky-18 <1033690636@qq.com>
2020-09-21 02:49:35 +0800

    add i18n, vuetify, and sass

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'zh',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}

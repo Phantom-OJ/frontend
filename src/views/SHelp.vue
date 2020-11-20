66161fdcf10b8f092728ac96bdf33169541276ee
Tloops <11813207@mail.sustech.edu.cn>
2020-11-20 17:11:56 +0800

    basic view for "Help" implemented

---
+++ src/views/SHelp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-all-root">
+    <s-help-card/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import SHelpCard from '@/components/SHelpCard.vue';
+
+@Component({
+  components: {SHelpCard}
+})
+export default class SHelp extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>

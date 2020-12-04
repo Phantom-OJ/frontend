8eadc3258c8305da1393dfbac6d0f7c3b80077f6
Tloops <11813207@mail.sustech.edu.cn>
2020-12-04 14:11:57 +0800

    "404" page new image!

--- src/components/SNotFoundCard.vue
+++ src/components/SNotFoundCard.vue
@@ -1,22 +1,27 @@
 <template>
   <v-card class="all-card ErrorPage-container">
-    <v-slide-x-transition>
     <div>
       <h1 class="Error-title">404</h1>
       <p class="Error-text">{{$t("not-found.text")}}</p>
       <v-btn color="secondary" @click="click()">{{$t("not-found.button")}}</v-btn>
     </div>
-    </v-slide-x-transition>
-    <v-slide-x-transition>
-      <v-img src="/404.jpg" class="ErrorPage-image"></v-img>
-    </v-slide-x-transition>
+    <v-img
+      src="/phantom_404.jpg"
+      class="ErrorPage-image"
+    ></v-img>
   </v-card>
 </template>
 
 <script>
 import {Vue} from '@/ts/extension'
 import {Component} from 'vue-property-decorator'
-@Component({})
+import {mapState} from "vuex";
+
+@Component({
+  computed:{
+    ...mapState(['width_height'])
+  }
+})
 export default class SNotFoundCard extends Vue{
   click() {
     this.$router.push(`/`)
@@ -26,7 +31,7 @@ export default class SNotFoundCard extends Vue{
 
 <style scoped>
   .ErrorPage-container{
-    padding: 90px;
+    padding: 80px 100px;
     display: flex;
     justify-content: space-around;
     align-items: center;
@@ -38,6 +43,7 @@ export default class SNotFoundCard extends Vue{
     font-size: 20px;
   }
   .ErrorPage-image{
-    max-width: 230px;
+    max-width: 270px;
+    border-radius: 2%;
   }
 </style>

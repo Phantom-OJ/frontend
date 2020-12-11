a95a8be121d3c46df8dd135437534a58485e3cdd
Star-Sky-18 <1033690636@qq.com>
2020-12-12 02:29:06 +0800

    [complement] add transition

--- src/components/General/SEntryList.vue
+++ src/components/General/SEntryList.vue
@@ -1,27 +1,30 @@
 <template>
   <v-list class="list problem-list">
-    <div
-      v-for="entry in entries"
-      :key="entry.ID"
-      class="list-item-container"
-    >
-      <v-list-item
-        class="list-item cursor-hand-hover"
-        @click="click(entry.ID)"
+    <transition-group name="slide-x-reverse-transition">
+      <div
+        v-for="entity in entities_"
+        :key="entity.ID"
+        class="list-item-container"
       >
-        <v-row justify="space-between" style="width: 100%;min-height: 70px;" align-content="center">
-          <slot :entry="entry"/>
-        </v-row>
-      </v-list-item>
-      <v-divider/>
-    </div>
+        <v-list-item
+          class="list-item cursor-hand-hover"
+          @click="click(entity.ID)"
+        >
+          <v-row justify="space-between" style="width: 100%;min-height: 70px;" align-content="center">
+            <slot :entry="entity"/>
+          </v-row>
+        </v-list-item>
+        <v-divider/>
+      </div>
+    </transition-group>
   </v-list>
 </template>
 
 <script lang="ts">
 import {Vue} from '@/ts/extension'
-import {Component, Prop} from 'vue-property-decorator'
+import {Component, Prop, Watch} from 'vue-property-decorator'
 import {Entity} from "@/ts/interfaces";
+import {SUtil} from "@/ts/utils";
 
 @Component({})
 export default class SEntryList extends Vue {
@@ -29,13 +32,26 @@ export default class SEntryList extends Vue {
     type: Array,
     required: true
   })
-  readonly entries!: Array<Entity>
+  readonly entities!: Array<Entity>
 
   @Prop({
     type: String,
     required: true
   })
   readonly path!: string
+
+  entities_:Entity[]=[]
+
+  @Watch('entities', {immediate:true})
+  async entityChange(){
+    while(this.entities_.length>0){
+      this.entities_.shift()
+    }
+    await SUtil.sleep(300)
+    for (let i of this.entities){
+      this.entities_.push(i)
+    }
+  }
 
   click(ID: number) {
     this.$router.push(`/${this.path}/${ID}`)
